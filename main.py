527dcf8c05edee96e9f68a752e6ea4a19e155e68
desigirlvideo <jiyarulislam533@gmail.com>
2026-09-09 11:04:00 +0600

    Update main.py

--- main.py
+++ main.py
@@ -14,9 +14,9 @@ from telegram.ext import (
 )
 
 # Configurations
-BOT_TOKEN = "8210193780:AAEvVqDvfNXvQ5lEAfo64DLOnvFNN59ima8"
+BOT_TOKEN = "8210193780:AAHgexSciAcNOhUnJkOhVxdj4Y5Mi576g-c"
 ADMIN_ID = 5899402664
-LOG_CHANNEL_ID = -1004483673752
+LOG_CHANNEL_ID = -1004483673752  # আপনার ১৩ সংখ্যার সঠিক Channel ID-টি এখানে চেক করে বসিয়ে নিন
 SUPPORT_USERNAME = "ziaulx"
 COIN_TRANSFER_USERNAME = "@ziaulx90"
 
